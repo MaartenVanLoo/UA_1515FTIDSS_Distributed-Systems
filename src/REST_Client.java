bdb26e923e7685c740fbd6683d5c365c233dcd5a
Maarten Van Loo <95285436+MaartenVanLoo@users.noreply.github.com>
2022-03-16 12:06:59 +0100

    Added main methods

--- src/REST_Client.java
+++ src/REST_Client.java
@@ -1,2 +1,10 @@
+import java.io.IOException;
+
 public class REST_Client {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting REST client");
+        System.out.println("Server host ip:");
+        IPUtils.printIpv4Interfaces();
+    }
 }
--- src/REST_Server.java
+++ src/REST_Server.java
@@ -1,2 +1,12 @@
+import java.io.IOException;
+
 public class REST_Server {
+
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting REST server");
+        System.out.println("Server host ip:");
+        IPUtils.printIpv4Interfaces();
+    }
 }
+
