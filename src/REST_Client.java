1afe076d6bcec8ed59dbd6eadba4d6d8b47cba48
Jens Verelst <jensverelst@gmail.com>
2022-03-16 17:25:44 +0100

    Added user input

--- src/REST_Client.java
+++ src/REST_Client.java
@@ -1,5 +1,7 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.net.URI;
@@ -7,6 +9,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
+
 //https://www.baeldung.com/java-9-http-client
 public class REST_Client {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -27,22 +31,44 @@ public class REST_Client {
                 .PUT(HttpRequest.BodyPublishers.ofString("200"))
                 .build();
         IPUtils.printIpv4Interfaces();
-        //synchronous
-        HttpResponse balance =
-                client.send(getBalance, BodyHandlers.ofString());
-        System.out.println(balance.statusCode());
-        System.out.println(balance.body());
 
-        HttpResponse add =
-                client.send(addMoney, BodyHandlers.ofString());
-        System.out.println(add.statusCode());
-        System.out.println(add.body());
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your command: ");
+        String inputLine = sc.nextLine();
+        //synchronous send
+        while (inputLine!= null) {
+            if(inputLine.equals("getBalance")){
+                HttpResponse balance =
+                        client.send(getBalance, BodyHandlers.ofString());
+                System.out.println(balance.statusCode());
+                System.out.println(balance.body());
 
-        HttpResponse receive =
-                client.send(getMoney, BodyHandlers.ofString());
-        System.out.println(receive.statusCode());
-        System.out.println(receive.body());
+            }
+            else if(inputLine.equals("addMoney")){
+                HttpResponse add =
+                        client.send(addMoney, BodyHandlers.ofString());
+                System.out.println(add.statusCode());
+                System.out.println(add.body());
 
+            }
+            else if(inputLine.equals("getMoney")){
+                HttpResponse receive =
+                        client.send(getMoney, BodyHandlers.ofString());
+                System.out.println(receive.statusCode());
+                System.out.println(receive.body());
+
+            }
+            else if(inputLine.equals("stop")){
+                System.out.println("Shutting down");
+                break;
+            }
+            else{
+                System.out.println("Unknown Command");
+
+            }
+            System.out.print("Enter your command: ");
+            inputLine = sc.nextLine();
+        }
     }
 
 }
